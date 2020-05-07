5836f1e64af89c0b3c2d5dfdb02ae5e4e5a71333
DgnrtnX <siddgage@gmail.com>
2020-05-07 16:15:15 +0530

    improvements

--- django_projects/blog/views.py
+++ django_projects/blog/views.py
@@ -1,29 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author':'sidd',
-        'title':'post no 1',
-        'content':'some content',
-        'date':'todays date'
-    },
-    {
-        'author':'sidd 1',
-        'title':'post no 2',
-        'content':'some content again',
-        'date':'todays date again'
-    },
-    {
-        'author':'sidd 2',
-        'title':'post no 3',
-        'content':'some content and again',
-        'date':'todays date and again'
-    },
-]
 # render return a httpResponse and is a shortcut method used here.
 def index(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html', context) 
 
