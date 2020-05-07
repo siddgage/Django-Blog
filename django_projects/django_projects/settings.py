833c767aa3198a9c7ef86e4cf1e16e24c2b22a13
DgnrtnX <siddgage@gmail.com>
2020-05-07 19:47:37 +0530

    added new media dir

--- django_projects/django_projects/settings.py
+++ django_projects/django_projects/settings.py
@@ -122,6 +122,11 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog_home'
+
