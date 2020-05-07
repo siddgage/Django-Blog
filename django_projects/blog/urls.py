6db30dd74abc4196e08748e68c97dde16ac54640
DgnrtnX <siddgage@gmail.com>
2020-05-07 18:31:16 +0530

    added log in and out path

--- django_projects/blog/urls.py
+++ django_projects/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.index,name="blog_home"),
     path('about/',views.about,name="blog_about"),
     path('register/',user_views.register,name="register"),
+    path('login/',auth_views.LoginView.as_view(),name="login"),
+    path('logout/',auth_views.LogoutView.as_view(),name="logput"),
 ]
