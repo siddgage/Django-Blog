c006fe04d2637abf39d0b91e7f2ef4abf1024166
DgnrtnX <siddgage@gmail.com>
2020-05-07 18:43:39 +0530

    changed admin url from /admin to /blog/admin

--- django_projects/blog/urls.py
+++ django_projects/blog/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 from users import views as user_views
@@ -5,6 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.index,name="blog_home"),
+    path('admin/', admin.site.urls),
     path('about/',views.about,name="blog_about"),
     path('register/',user_views.register,name="register"),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name="login"),
