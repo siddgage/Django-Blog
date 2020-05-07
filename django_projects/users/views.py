b9a99b35e6407e4e3aee5254dc6c73a57ffe6160
DgnrtnX <siddgage@gmail.com>
2020-05-07 20:25:41 +0530

    added various updatation forms

--- django_projects/users/views.py
+++ django_projects/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, ProfileUpdatationForm, UserUpdatationForm
 
 def register(request):
     if request.method =='POST':
@@ -18,4 +18,10 @@ def register(request):
     return render(request, 'users/register.html',{'form': form})
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    update_form = UserUpdatationForm()
+    profile_update = ProfileUpdatationForm()
+    context = {
+        'update_form': update_form,
+        'profile_update': profile_update,
+    }
+    return render(request,'users/profile.html', context)
