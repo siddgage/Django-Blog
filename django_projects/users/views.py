543b1b6a8071f5f7292205d19640881ec7266af0
DgnrtnX <siddgage@gmail.com>
2020-05-07 17:38:59 +0530

    saving user

--- django_projects/users/views.py
+++ django_projects/users/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 
 def register(request):
     if request.method =='POST':
+        form.save()
         form = UserCreationForm(request.POST)
         if form.is_valid():
             username= form.cleaned_data.get('username')
