c7594b94e422d2512896314716c941f95e516d9e
DgnrtnX <siddgage@gmail.com>
2020-05-07 16:37:14 +0530

    updated register path

--- django_projects/blog/urls.py
+++ django_projects/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from users import views as user_views
 
 urlpatterns = [
     path('',views.index,name="blog_home"),
-    path('about/',views.about,name="blog_about")
+    path('about/',views.about,name="blog_about"),
+    path('register/',user_views.register,name="register"),
 ]
