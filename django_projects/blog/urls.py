5bfc8b1d54c0aab07cc1a6640ba38652cc1bfcef
DgnrtnX <siddgage@gmail.com>
2020-05-07 15:17:25 +0530

    statics and tem updated

--- django_projects/blog/urls.py
+++ django_projects/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="blog | home"),
+    path('',views.index,name="blog_home"),
+    path('about/',views.about,name="blog_about")
 ]
--- django_projects/blog/views.py
+++ django_projects/blog/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
-
+posts = [
+    {
+        'author':'sidd',
+        'title':'post no 1',
+        'content':'some content',
+        'date':'todays date'
+    },
+    {
+        'author':'sidd 1',
+        'title':'post no 2',
+        'content':'some content again',
+        'date':'todays date again'
+    },
+    {
+        'author':'sidd 2',
+        'title':'post no 3',
+        'content':'some content and again',
+        'date':'todays date and again'
+    },
+]
+# render return a httpResponse and is a shortcut method used here.
 def index(request):
-    return render(request,'blog/home')
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html', context) 
 
 
 def about(request):
-    return render(request,'blog/about')
+    return render(request,'blog/about.html', {'title':'BLOG | ABOUT'})
