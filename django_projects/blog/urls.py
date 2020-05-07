d457084c0943f11501d18018e572ece21358eac3
DgnrtnX <siddgage@gmail.com>
2020-05-07 18:33:00 +0530

    change template path

--- django_projects/blog/urls.py
+++ django_projects/blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('',views.index,name="blog_home"),
     path('about/',views.about,name="blog_about"),
     path('register/',user_views.register,name="register"),
-    path('login/',auth_views.LoginView.as_view(),name="login"),
-    path('logout/',auth_views.LogoutView.as_view(),name="logput"),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name="login"),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name="logput"),
 ]
