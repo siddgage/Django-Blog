c277fef33bd940fb72b975aa60f1475f4a77aa11
DgnrtnX <siddgage@gmail.com>
2020-05-08 16:05:38 +0530

    reset msg page

--- django_projects/blog/urls.py
+++ django_projects/blog/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name="login"),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name="logout"),
     path('password_reset/',auth_views.PasswordResetView.as_view(template_name='users/pass_reset.html'),name="password_reset"),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/pass_reset_done.html'),name="password_reset_done"),
     path('profile/',user_views.profile,name="profile"),
 ]
